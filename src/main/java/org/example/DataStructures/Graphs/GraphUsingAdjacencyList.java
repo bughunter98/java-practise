96b72b45ec25b19f087467d365015f38b472d392
bughunter98 <saitejanaiduthota@gmail.com>
2025-04-21 16:56:36 +0530

    added DFS .

--- src/main/java/org/example/DataStructures/Graphs/GraphUsingAdjacencyList.java
+++ src/main/java/org/example/DataStructures/Graphs/GraphUsingAdjacencyList.java
@@ -2,6 +2,8 @@ package org.example.DataStructures.Graphs;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
 
 public class GraphUsingAdjacencyList {
     ArrayList<GraphNodeList> nodeList = new ArrayList<>();
@@ -42,6 +44,38 @@ public class GraphUsingAdjacencyList {
             }
         }
     }
+    private void dfs() {
+        // as BFS already triggered before this , all the isVisited would have been true . so making them false
+        // ipudu paina already bfs method main method lo call chesa kabatti anni noded ki isVisited true aipotadi so
+        // adhi false cheyalo kabati kinda foreachloop raasa
+        nodeList.forEach(node -> {
+            if (node.isVisited) {
+                node.isVisited=false;
+            }}
+        );
+        for (GraphNodeList node : nodeList){
+            if (!node.isVisited){
+                dfsVisit(node);
+            }
+        }
+    }
+
+    private void dfsVisit(GraphNodeList node) {
+        Stack<GraphNodeList> stack = new Stack<>();
+        stack.push(node);
+        while (!stack.isEmpty()){
+            GraphNodeList currentNode = stack.pop();
+            currentNode.isVisited=true;
+            System.out.print(currentNode.name+" ");
+            ArrayList<GraphNodeList> neighBours = currentNode.neighbours;
+            for (GraphNodeList neighBour : neighBours){
+                if (!neighBour.isVisited){
+                    stack.add(neighBour);
+                    neighBour.isVisited=true;
+                }
+            }
+        }
+    }
 
     private void bfsVisit(GraphNodeList node) {
         LinkedList<GraphNodeList> queue = new LinkedList<>();
@@ -58,25 +92,63 @@ public class GraphUsingAdjacencyList {
                 }
             }
         }
+        // BFS time complexity is O(V+E) . same goes with spacce complexity as well.
     }
 
     public static void main(String[] args) {
+        ArrayList<GraphNodeList> nodeList = new ArrayList<>();
+        nodeList.add(new GraphNodeList("A", 0));
+        nodeList.add(new GraphNodeList("B", 1));
+        nodeList.add(new GraphNodeList("C", 2));
+        nodeList.add(new GraphNodeList("D", 3));
+        nodeList.add(new GraphNodeList("E", 4));
+
+        GraphUsingAdjacencyList graph = new GraphUsingAdjacencyList(nodeList);
+        graph.addUnDirectedEdge(0, 1);
+        graph.addUnDirectedEdge(0, 2);
+        graph.addUnDirectedEdge(0, 3);
+        graph.addUnDirectedEdge(2, 3);
+        graph.addUnDirectedEdge(1, 4);
+        graph.addUnDirectedEdge(3, 4);
+
+        System.out.println(graph.printAdjacencyList());
+        System.out.println("Breadth First Search ");
+        graph.bfs();
+        System.out.println();
+        System.out.println("Depth First Search");
+        graph.dfs();
+        System.out.println();
+
+        ArrayList<GraphNodeList> nodeList1 = getNodeList();
+
+        GraphUsingAdjacencyList graph1 = new GraphUsingAdjacencyList(nodeList1);
+        graph1.addUnDirectedEdge(0, 1);
+        graph1.addUnDirectedEdge(0, 2);
+        graph1.addUnDirectedEdge(1, 3);
+        graph1.addUnDirectedEdge(1, 6);
+        graph1.addUnDirectedEdge(2, 3);
+        graph1.addUnDirectedEdge(2, 4);
+        graph1.addUnDirectedEdge(3, 5);
+        graph1.addUnDirectedEdge(4, 5);
+        graph1.addUnDirectedEdge(5, 6);
+        System.out.println(graph1.printAdjacencyList());
+
+        System.out.println("Breadth First Search ");
+        graph1.bfs();
+        System.out.println();
+        System.out.println("Depth First Search");
+        graph1.dfs();
+
+    }
+    private static ArrayList<GraphNodeList> getNodeList() {
         ArrayList<GraphNodeList> nodeList = new ArrayList<>();
         nodeList.add(new GraphNodeList("A",0));
         nodeList.add(new GraphNodeList("B",1));
         nodeList.add(new GraphNodeList("C",2));
         nodeList.add(new GraphNodeList("D",3));
         nodeList.add(new GraphNodeList("E",4));
-
-        GraphUsingAdjacencyList graph = new GraphUsingAdjacencyList(nodeList);
-        graph.addUnDirectedEdge(0,1);
-        graph.addUnDirectedEdge(0,2);
-        graph.addUnDirectedEdge(0,3);
-        graph.addUnDirectedEdge(2,3);
-        graph.addUnDirectedEdge(1,4);
-        graph.addUnDirectedEdge(3,4);
-
-        System.out.println(graph.printAdjacencyList());
-        graph.bfs();
+        nodeList.add(new GraphNodeList("F",5));
+        nodeList.add(new GraphNodeList("G",6));
+        return nodeList;
     }
 }
--- src/main/java/org/example/DataStructures/Graphs/GraphUsingAdjacencyMatrix.java
+++ src/main/java/org/example/DataStructures/Graphs/GraphUsingAdjacencyMatrix.java
@@ -3,6 +3,7 @@ package org.example.DataStructures.Graphs;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 
 public class GraphUsingAdjacencyMatrix {
     List<GraphNode> nodeList;
@@ -44,6 +45,39 @@ public class GraphUsingAdjacencyMatrix {
                 bfsVisit(node);
             }
 
+        }
+    }
+
+    public void dfs(){
+        // as BFS already triggered before this , all the isVisited would have been true . so making them false
+        // ipudu paina already bfs method main method lo call chesa kabatti anni noded ki isVisited true aipotadi so
+        // adhi false cheyalo kabati kinda foreachloop raasa
+        nodeList.forEach(node -> {
+            if (node.isVisited) {
+                node.isVisited=false;
+            }}
+        );
+        for (GraphNode node : nodeList){
+            if (!node.isVisited){
+                dfsVisit(node);
+            }
+        }
+    }
+
+    private void dfsVisit(GraphNode node){
+        Stack<GraphNode> stack = new Stack<>();
+        stack.add(node);
+        while (!stack.isEmpty()){
+            GraphNode currentNode = stack.pop();
+            currentNode.isVisited=true;
+            System.out.print(currentNode.name+" ");
+            ArrayList<GraphNode> neighBours = getNeighbours(currentNode);
+            for (GraphNode neighbour : neighBours){
+                if (!neighbour.isVisited){
+                    stack.push(neighbour);
+                    neighbour.isVisited=true;
+                }
+            }
         }
     }
 
@@ -94,7 +128,44 @@ public class GraphUsingAdjacencyMatrix {
         graph.addUndirectedEdge(1,4);
         graph.addUndirectedEdge(3,4);
         System.out.println(graph.printAdjacencyMatrix());
+        System.out.println("Breadth First Search ");
         graph.bfs();
+        System.out.println();
+        System.out.println("Depth First Search");
+        graph.dfs();
+        System.out.println();
 
+        List<GraphNode> nodeList1 = getNodeList();
+
+        GraphUsingAdjacencyMatrix graph1 = new GraphUsingAdjacencyMatrix(nodeList1);
+        graph1.addUndirectedEdge(0,1);
+        graph1.addUndirectedEdge(0,2);
+        graph1.addUndirectedEdge(1,3);
+        graph1.addUndirectedEdge(1,6);
+        graph1.addUndirectedEdge(2,3);
+        graph1.addUndirectedEdge(2,4);
+        graph1.addUndirectedEdge(3,5);
+        graph1.addUndirectedEdge(4,5);
+        graph1.addUndirectedEdge(5,6);
+        System.out.println(graph1.printAdjacencyMatrix());
+
+        System.out.println("Breadth First Search ");
+        graph1.bfs();
+        System.out.println();
+        System.out.println("Depth First Search");
+        graph1.dfs();
+
+    }
+
+    private static List<GraphNode> getNodeList() {
+        List<GraphNode> nodeList = new ArrayList<>();
+        nodeList.add(new GraphNode("A",0));
+        nodeList.add(new GraphNode("B",1));
+        nodeList.add(new GraphNode("C",2));
+        nodeList.add(new GraphNode("D",3));
+        nodeList.add(new GraphNode("E",4));
+        nodeList.add(new GraphNode("F",5));
+        nodeList.add(new GraphNode("G",6));
+        return nodeList;
     }
 }
